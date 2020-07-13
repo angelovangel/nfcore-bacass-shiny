c30fae5779934e360d1909d2c94de14bc450b1c4
Angel <aangeloo@gmail.com>
2020-07-13 13:54:57 +0200

    add modal for ncct report header

---
+++ ncct_make_yaml.R
@@ -0,0 +1,34 @@
+# generate multiqc_config.yaml file from modal inputs
+# entries match inputs from ncct_modal.R
+# passed as arg to nextflow-bcl (--multiqc_config file.yaml)
+
+# when the yaml file is passed to multiqc with -c, the report will have this custom header
+# https://multiqc.info/docs/#customising-reports
+
+require(yaml)
+
+ncct_make_yaml <- function(customer = "", 
+                           project_id = "", 
+                           ncct_contact = "", 
+                           project_type = "", 
+                           lib_prep = "", 
+                           indexing = "", 
+                           seq_setup = "", 
+                           ymlfile = "temp.yml") {
+  write_yaml(indent.mapping.sequence = TRUE, 
+             indent = 2, 
+             file = ymlfile,
+    list(
+      intro_text = FALSE,
+      report_header_info = list(
+        list("Customer" = customer),
+        list("Project" = project_id),
+        list("NCCT contact" = ncct_contact),
+        list("Project type" = project_type),
+        list("Library prep kit" = lib_prep),
+        list("Index kit" = indexing),
+        list("Sequencing setup" = seq_setup)
+      )
+    )
+  )
+}
